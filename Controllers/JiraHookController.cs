f601e1960ba5162c4de8b61189cc2dc62dbd5afd
Jolyon Wharton <jolyon.wharton@newday.co.uk>
2021-04-26 09:51:30 +0100

    C# Project added

---
+++ Controllers/JiraHookController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GhJiraIntegration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JiraHookController : ControllerBase
+    {
+        public IActionResult Create()
+        {
+            return Ok("Works");
+        }
+    }
+}
